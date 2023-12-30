e6016e6ce7f66da95756b6016e71e591265423d8
graham-livingston <glivingston@uchicago.edu>
2023-12-29 20:09:53 -0600

    search demo

--- js/main.js
+++ js/main.js
@@ -9,11 +9,14 @@ $(document).ready(function() {
     $('#searchBar').keypress(function(event) {
         var keycode = (event.keyCode ? event.keyCode : event.which);
         if(keycode == '13') {
-          console.log($(this).val());
-          $(this).val('');
-          event.preventDefault(); // Prevent form submission
+             var input = $(this).val();
+      $(this).val('');
+      event.preventDefault(); // Prevent form submission
+      makeAPICall(input);
         }
       });
+
+      
 
     $.getJSON('./interviews.json', function(data) {
       let div = $('#interviewsContainer');
@@ -27,3 +30,21 @@ $(document).ready(function() {
       });
     });
   });
+
+  function makeAPICall(input) {
+    $.getJSON('./search.json', function(data) {
+
+      // Clear the previous response
+      $('#resultsContainer').empty();
+
+
+      // Assuming the response data is an array of items
+      $.each(data.interviews, function(i, interview) {
+        var name = interview.name;
+        var summary = interview.summary;
+        var link = interview.link;
+        var usrInput = input;
+        $('#resultsContainer').append('<p>Name: ' + name + '</p><p>Summary: ' + summary + '</p><a href="' + link + '" target="_blank">Interview Link</a><p>User Input: ' + usrInput + '</p><br>');
+      });
+    });
+  }
